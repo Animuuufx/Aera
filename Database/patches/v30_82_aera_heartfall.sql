ad829d24dc888d3ead75c24e6d9ee8d5f79d7116
Animuuufx <nightvaults@gmail.com>
2026-09-11 21:31:45 +0000

    Remove retired Heartfall installer

--- Database/patches/v30_82_aera_heartfall.sql
+++
@@ -1,6 +0,0 @@
--- RETIRED: old 20-map Heartfall installer.
--- Some of its map SWFs were not compatible with the current AS3 client.
--- Do not install this migration.
--- Use Database/patches/v30_84_aera_heartfall_reset_10map.sql instead.
-
-SELECT 'v30_82 retired - import v30_84_aera_heartfall_reset_10map.sql' AS `Heartfall`;
