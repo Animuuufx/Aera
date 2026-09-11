dfdf61ae1482861c4f0ffd35a81612b13bf2194c
Animuuufx <nightvaults@gmail.com>
2026-09-11 21:31:49 +0000

    Remove retired Heartfall polish migration

--- Database/patches/v30_83_aera_heartfall_polish.sql
+++
@@ -1,6 +0,0 @@
--- RETIRED: polish migration for the removed 20-map Heartfall installer.
--- Do not install this migration.
--- The replacement 10-map storyline is installed by:
--- Database/patches/v30_84_aera_heartfall_reset_10map.sql
-
-SELECT 'v30_83 retired - import v30_84_aera_heartfall_reset_10map.sql' AS `Heartfall`;
